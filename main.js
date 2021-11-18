09032e51be1ade23bf091e24acf8cd8e514a3723
julesrx <25800503+julesrx@users.noreply.github.com>
2021-11-18 23:52:48 +0100

    speed

--- main.js
+++ main.js
@@ -2,32 +2,30 @@ import './style.css';
 
 const icons = [];
 
-let xspeed, yspeed;
-
 let width = window.innerWidth;
 let height = window.innerHeight;
 
 const frame = () => {
-  for (let icon of icons) {
-    icon.x = icon.x + xspeed;
-    icon.y = icon.y + yspeed;
+  for (const icon of icons) {
+    icon.x = icon.x + icon.xspeed;
+    icon.y = icon.y + icon.yspeed;
 
-    icon.el.style.left = icon.x + 'px';
-    icon.el.style.top = icon.y + 'px';
+    icon.el.style.left = `${icon.x}px`;
+    icon.el.style.top = `${icon.y}px`;
 
     if (icon.x + icon.el.clientWidth >= width) {
-      xspeed = -xspeed;
+      icon.xspeed = -icon.xspeed;
       icon.x = width - icon.el.clientWidth;
     } else if (icon.x <= 0) {
-      xspeed = -xspeed;
+      icon.xspeed = -icon.xspeed;
       icon.x = 0;
     }
 
     if (icon.y + icon.el.clientHeight >= height) {
-      yspeed = -yspeed;
-      icon.y = height -  icon.el.clientHeight;
+      icon.yspeed = -icon.yspeed;
+      icon.y = height - icon.el.clientHeight;
     } else if (icon.y <= 0) {
-      yspeed = -yspeed;
+      icon.yspeed = -icon.yspeed;
       icon.y = 0;
     }
   }
@@ -35,15 +33,13 @@ const frame = () => {
 
 const setup = () => {
   for (const svg of document.getElementsByTagName('svg')) {
-    icons.push({ el: svg, x: 0, y: 0 });
+    icons.push({ el: svg, x: random(width), y: random(height), xspeed: 2, yspeed: 2 });
   }
-
-  xspeed = 1;
-  yspeed = 1;
 
   setInterval(frame, fps(120));
 };
 
+const random = max => Math.floor(Math.random() * max) + 1;
 const fps = n => Math.floor((1 / n) * 1000);
 
 setup();
