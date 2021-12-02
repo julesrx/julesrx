bdf5e106b37df71647e3cd965eaba642d32d0b23
julesrx <25800503+julesrx@users.noreply.github.com>
2021-12-02 23:57:16 +0100

    noopener noreferrer

--- readme.js
+++ readme.js
@@ -7,8 +7,16 @@ const index = path.join(__dirname, 'dist', 'index.html');
 fs.readFile('./README.md', (_, readme) => {
   fs.readFile(index, (_, f) => {
     const html = f.toString();
-    const render = marked.parse(readme.toString());
 
-    fs.writeFile(index, html.replace('{{readme}}', render), () => {});
+    marked.use({
+      renderer: {
+        link: function () {
+          const link = marked.Renderer.prototype.link.apply(this, arguments);
+          return link.replace('<a', '<a rel="noopener noreferrer"');
+        }
+      }
+    });
+
+    fs.writeFile(index, html.replace('{{readme}}', marked.parse(readme.toString())), () => {});
   });
 });
