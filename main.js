ed454978e5be923adee9d5b894534cf61db791cc
julesrx <25800503+julesrx@users.noreply.github.com>
2021-12-03 00:51:45 +0100

    insert emojis wia javascript

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ import 'virtual:windi.css';
 import './main.css';
 import Bouncer from 'bouncing-element';
 
+Array(3)
+  .fill(['🐧', '🌱', '👨‍💻', '🎮', '🍝', '🌈', '📀', '🏍', '🐸'])
+  .flat()
+  .map(e => {
+    const i = document.createElement('i');
+    i.className = 'bounce';
+    i.innerHTML = e;
+    return i;
+  })
+  .forEach(e => document.body.appendChild(e));
+
 new Bouncer();
