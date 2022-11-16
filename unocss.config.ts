33d79f232285bcc4389b3ad674f60c5583cc57f5
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2022-11-16 13:56:07 +0100

    Update unocss.config.ts

--- unocss.config.ts
+++ unocss.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       sans: ['Inter', uno.theme!.fontFamily!.sans]
     }
   },
-  shortcuts: { bounce: 'h-6 w-6 opacity-20 sm:opacity-40 lg:opacity-100 rounded-full' },
+  shortcuts: { bounce: 'h-6 w-6 opacity-20 sm:opacity-40 lg:opacity-100 rounded-full -z-1' },
   presets: [
     uno,
     presetTypography({
@@ -20,7 +20,9 @@ export default defineConfig({
           'text-decoration': 'none'
         },
         code: {
-          background: 'rgba(110,118,129,0.4) !important'
+          background: 'rgba(110,118,129,0.4) !important',
+          'border-radius': '6px',
+          'font-family': uno.theme!.fontFamily!.mono,
         },
         img: {
           'max-width': '95vw',
