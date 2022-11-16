cbbe2dfccdde16b3f36a69dc17a023aa0190b530
julesrx <25800503+julesrx@users.noreply.github.com>
2022-11-17 00:40:01 +0100

    header

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
-<script setup lang="ts">
-const config = useAppConfig();
-</script>
-
 <template>
-  <img src="/favicon.ico" class="h-24 w-24 rounded-full mb-4" :alt="config.title" />
-
-  <ContentDoc
-    id="readme"
-    path="/readme"
-    tag="article"
-    class="prose color-[#f0f6fc]"
-    :head="false"
-  />
+  <ContentDoc id="readme" path="/readme" tag="article" class="prose" :head="false" />
 </template>
