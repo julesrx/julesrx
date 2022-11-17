904250c57c1fbff290422505079fdc3eeb77e23f
julesrx <25800503+julesrx@users.noreply.github.com>
2022-11-17 01:07:48 +0100

    typeCheck

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@unocss/nuxt'],
-  css: ['@fontsource/inter', '@unocss/reset/tailwind.css', '@/assets/main.scss']
+  css: ['@fontsource/inter', '@unocss/reset/tailwind.css', '@/assets/main.scss'],
+  typescript: { typeCheck: true, strict: true }
 });
