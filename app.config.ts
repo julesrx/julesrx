da31e5d8b83be5ab85b3366e26feb338d6480ccf
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2022-11-17 09:40:06 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,5 @@ export default defineAppConfig({
   url: 'https://julesrx.fr/',
   title: 'Jules Raffoux',
   description:
-    "I'm a full stack dev at bewease in Bordeaux, France 🇫🇷, working with Vue, Typescript, .NET and other recent stuff."
+    "I'm Jules, a full stack developer at bewease in Bordeaux, France 🇫🇷, working with Vue, Typescript, .NET and other recent stuff."
 });
