7504b3194270198c540186ca90986595c048f3d5
julesrx <25800503+julesrx@users.noreply.github.com>
2023-01-12 18:35:39 +0100

    meta in app config

--- app.vue
+++ app.vue
@@ -10,35 +10,6 @@ useBouncer();
     class="bg-[#0d1117] color-[#c9d1d9] lg:px-14 lg:pt-14 md:px-9 md:pt-9 px-2 pt-2 m-0 overflow-x-hidden"
   />
 
-  <Head>
-    <Meta name="title" :content="config.title" />
-    <Meta name="description" :content="config.description" />
-    <Meta name="viewport" content="width=device-width, initial-scale=1" />
-    <Meta name="theme-color" content="#2ecc71" />
-    <Meta name="apple-mobile-web-app-status-bar-style" content="default" />
-
-    <!-- Twitter -->
-    <Meta name="twitter:card" content="summary" />
-    <Meta name="twitter:site" content="@julesrafx" />
-    <Meta name="twitter:title" :content="config.title" />
-    <Meta name="twitter:description" :content="config.description" />
-    <Meta name="twitter:image" :content="config.url + 'img/summary.png'" />
-
-    <!-- Open Graph / Facebook -->
-    <Meta property="og:title" :content="config.title" />
-    <Meta property="og:type" content="website" />
-    <Meta property="og:image" :content="config.url + 'img/summary.png'" />
-    <Meta property="og:url" :content="config.url" />
-    <Meta property="og:description" :content="config.description" />
-    <Meta property="og:locale" content="fr_FR" />
-
-    <Link rel="canonical" href="url" />
-    <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    <Link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-
-    <Title>{{ config.title }}</Title>
-  </Head>
-
   <main>
     <header class="flex items-end mb-4 space-x-4 justify-between">
       <NuxtLink to="/">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,44 @@
+const title = 'Jules Raffoux';
+const description =
+  "I'm Jules, a full stack developer at bewease in Bordeaux, France 🇫🇷, working with Vue, Typescript, .NET and other recent stuff.";
+const url = 'https://julesrx.fr/';
+
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@unocss/nuxt'],
   css: ['@fontsource/inter', '@unocss/reset/tailwind.css', '@/assets/main.scss'],
   typescript: { typeCheck: true, strict: true },
-  appConfig: {
-    url: 'https://julesrx.fr/',
-    title: 'Jules Raffoux',
-    description:
-      "I'm Jules, a full stack developer at bewease in Bordeaux, France 🇫🇷, working with Vue, Typescript, .NET and other recent stuff."
+  appConfig: { title },
+  app: {
+    head: {
+      htmlAttrs: { lang: 'fr' },
+      title,
+      meta: [
+        { name: 'title', content: title },
+        { name: 'description', content: 'config.description' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#2ecc71' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+
+        // Twitter
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@julesrafx' },
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:image', content: url + 'img/summary.png' },
+
+        // Open Graph / Facebook
+        { name: 'og:title', content: title },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:image', content: url + 'img/summary.png' },
+        { name: 'og:url', content: url },
+        { name: 'og:description', content: description },
+        { name: 'og:locale', content: 'fr_FR' }
+      ],
+      link: [
+        { rel: 'canonical', href: url },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
   }
 });
