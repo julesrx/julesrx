b596b23a49cb7a4c806fcf1b6d51dcb1088134f8
julesrx <25800503+julesrx@users.noreply.github.com>
2023-06-14 01:19:08 +0200

    style

--- app.vue
+++ app.vue
@@ -9,8 +9,21 @@ useBouncer();
       <NuxtLink to="/">
         <img src="/jules.png" :alt="config.title" />
       </NuxtLink>
+
+      <NuxtLink to="/music" class="music">🎶</NuxtLink>
     </header>
 
     <NuxtPage />
   </main>
 </template>
+
+<style scoped lang="scss">
+header {
+  position: relative;
+
+  .music {
+    position: absolute;
+    right: 0;
+  }
+}
+</style>
