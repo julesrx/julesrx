e2e83eca5720ae52c82177e2b0081a75bf44235b
julesrx <25800503+julesrx@users.noreply.github.com>
2023-06-13 21:44:32 +0200

    notes

---
+++ music.ts
@@ -0,0 +1,107 @@
+/*
+rhcp chorsow 2007
+jagged little pill
+noonate cinq
+positions
+AMOK
+hey u x
+you come and go like a pop song
+happier than ever
+debut gjork
+post 
+twilight boa
+post human survival horror
+excursions
+life changing moment
+mionecraft volumes alpha and beta
+because the internet
+c-girl
+parachutees
+discovery
+random acces memory
+tron
+alive 2007
+rise and fall of ziggy stardust
+nineteen desired
+hotel california
+demon days
+plastic beach
+make yourself
+head of nasa and the 2 amish boys
+grace
+nectar joji
+in tongues
+hounds of love
+lucid lookas
+DSVII M83
+summer touch
+Halo 2 vol 1 & 2
+all of me 
+seychelles
+collected massive attack
+mezzanine
+heroes and vollains
+AIM
+Destiny taken king
+music of the spheres
+MVP mister v
+modjo
+2nd law
+showbiz
+origin of symetry
+hullaballoo
+pink moon
+nicole
+des viages des figures
+perdu d'avance
+la fête est finie
+animals
+dark side of the moon
+wish you were here
+the wall
+to hell with it
+to be one with you
+nurture
+dummy
+when marnie was there
+a moon shaped pool
+pablo honney
+kid a mnesia
+in rainbows
+spectre radiohead
+return of the dream canteen
+mother's milk
+blood sugar sex magic
+the gateway
+by the way
+stadium arcadium
+the red red velvet
+dogma resistance
+draining love story
+we are not your kind
+a light for attracting attention
+bloodsport
+squaring the circle
+becoming x
+virtual paradire
+plastic memories
+mignonne
+orphic
+hoodie tiffi
+the eraser
+sunshine kitty
+wueen of the clouds
+lady wood
+astro world
+v vald
+memories ost
+after hours
+lately i feel everything
+the unforgiving
+ep2
+record
+bae yung bae
+mirai addiction
+*/
+
+type Album = { title: string; artist: string; link: string; poster: string };
