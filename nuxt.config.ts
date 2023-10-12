e30f5e3b932739e63ee83304a51b1129d97aad5f
julesrx <25800503+julesrx@users.noreply.github.com>
2023-10-12 15:41:30 +0200

    mono font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,12 @@ const url = 'https://julesrx.fr/';
 
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/image'],
-  css: ['@fontsource-variable/rubik', 'chota/dist/chota.min.css', '@/assets/main.scss'],
+  css: [
+    '@fontsource-variable/rubik',
+    '@fontsource-variable/jetbrains-mono',
+    'chota/dist/chota.min.css',
+    '@/assets/main.scss'
+  ],
   typescript: { typeCheck: true, strict: true },
   appConfig: { title },
   app: {
