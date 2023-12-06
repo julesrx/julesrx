d17a21b284977889bf5de558304f4f6bf260bed6
julesrx <dev@julesrx.fr>
2023-12-06 11:33:08 +0100

    feat: update bouncers

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <script setup lang="ts">
-useBouncer();
+import { createBouncer } from 'bouncing-element';
+
+onMounted(() => {
+    const emojis = ['🐧', '🌱', '👨‍💻', '🎮', '🍝', '🌈', '📀', '🎬', '🏍', '🐸', '🚀'];
+    const els: HTMLElement[] = [
+        ...Array(3)
+            .fill(emojis)
+            .flat()
+            .map(e => {
+                const i = document.createElement('span');
+                i.className = 'bounce';
+                i.innerHTML = e;
+                return i;
+            }),
+        ...Array(3)
+            .fill('/favicon.ico')
+            .map(e => {
+                const i = document.createElement('img');
+                i.className = 'bounce';
+                i.src = e;
+                return i;
+            })
+    ];
+
+    const { start, stop } = createBouncer(els, { start: false, insert: true });
+
+    const preferredMotion = usePreferredReducedMotion();
+    watch(preferredMotion, motion => (motion === 'reduce' ? stop() : start()), { immediate: true });
+
+    onUnmounted(stop);
+});
 
 const { data } = await useFetch<{ login: string; avatar_url: string }>(
     'https://api.github.com/users/julesrx'
@@ -7,7 +37,7 @@ const { data } = await useFetch<{ login: string; avatar_url: string }>(
 </script>
 
 <template>
-    <main class="animate-fade-in">
+    <main>
         <Html class="dark" />
         <Body class="bg-background font-white px-2 pt-2 sm:px-9 sm:pt-9 lg:px-14 lg:pt-14" />
 
