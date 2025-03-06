23011a85d058b063f4ab4152dc229f8be3c8de94
julesrx <dev@julesrx.fr>
2025-03-06 21:45:46 +0100

    prettier

--- app.vue
+++ app.vue
@@ -2,59 +2,59 @@
 import { createBouncer } from 'bouncing-element';
 
 onMounted(() => {
-    const emojis = ['🐧', '🌱', '👨‍💻', '🎮', '🍝', '🌈', '📀', '🎬', '🏍', '🐸', '🚀'];
-    const els: HTMLElement[] = [
-        ...Array(3)
-            .fill(emojis)
-            .flat()
-            .map(e => {
-                const i = document.createElement('span');
-                i.className = 'bounce';
-                i.innerHTML = e;
-                return i;
-            }),
-        ...Array(3)
-            .fill('/favicon.ico')
-            .map(e => {
-                const i = document.createElement('img');
-                i.className = 'bounce';
-                i.alt = 'Favicon';
-                i.src = e;
-                return i;
-            })
-    ];
+  const emojis = ['🐧', '🌱', '👨‍💻', '🎮', '🍝', '🌈', '📀', '🎬', '🏍', '🐸', '🚀'];
+  const els: HTMLElement[] = [
+    ...Array(3)
+      .fill(emojis)
+      .flat()
+      .map(e => {
+        const i = document.createElement('span');
+        i.className = 'bounce';
+        i.innerHTML = e;
+        return i;
+      }),
+    ...Array(3)
+      .fill('/favicon.ico')
+      .map(e => {
+        const i = document.createElement('img');
+        i.className = 'bounce';
+        i.alt = 'Favicon';
+        i.src = e;
+        return i;
+      })
+  ];
 
-    const { start, stop } = createBouncer(els, { start: false, insert: true });
+  const { start, stop } = createBouncer(els, { start: false, insert: true });
 
-    const preferredMotion = usePreferredReducedMotion();
-    watch(preferredMotion, motion => (motion === 'reduce' ? stop() : start()), { immediate: true });
+  const preferredMotion = usePreferredReducedMotion();
+  watch(preferredMotion, motion => (motion === 'reduce' ? stop() : start()), { immediate: true });
 
-    onUnmounted(stop);
+  onUnmounted(stop);
 });
 
 const { data } = await useFetch<{ login: string; avatar_url: string }>(
-    'https://api.github.com/users/julesrx'
+  'https://api.github.com/users/julesrx'
 );
 </script>
 
 <template>
-    <main>
-        <Html class="dark" />
-        <Body class="bg-background font-white px-2 pt-2 sm:px-6 sm:pt-6 lg:px-10 lg:pt-10" />
+  <main>
+    <Html class="dark" />
+    <Body class="bg-background font-white px-2 pt-2 sm:px-6 sm:pt-6 lg:px-10 lg:pt-10" />
 
-        <header class="z-10 relative inline-block">
-            <NuxtLink to="/" v-if="data">
-                <NuxtImg
-                    :src="data.avatar_url.replace(/(\?|%3Fu).*$/, '')"
-                    format="webp"
-                    height="200"
-                    width="200"
-                    :alt="data.login"
-                    class="rounded-full h-40 w-40"
-                />
-            </NuxtLink>
-        </header>
+    <header class="z-10 relative inline-block">
+      <NuxtLink to="/" v-if="data">
+        <NuxtImg
+          :src="data.avatar_url.replace(/(\?|%3Fu).*$/, '')"
+          format="webp"
+          height="200"
+          width="200"
+          :alt="data.login"
+          class="rounded-full h-40 w-40"
+        />
+      </NuxtLink>
+    </header>
 
-        <NuxtPage />
-    </main>
+    <NuxtPage />
+  </main>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,46 +5,46 @@ import { theme } from '@julesrx/utils';
 import typography from '@tailwindcss/typography';
 
 const config: Config = {
-    darkMode: 'class',
-    content: [
-        './components/**/*.{js,vue,ts}',
-        './layouts/**/*.vue',
-        './pages/**/*.vue',
-        './plugins/**/*.{js,ts}',
-        './app.vue',
-        './error.vue',
-        './nuxt.config.ts'
-    ],
-    plugins: [typography],
-    theme: {
-        extend: {
-            colors: {
-                primary: theme.primary,
-                secondary: 'hsla(215,8%,47%,.4)',
-                white: theme.body,
-                background: theme.background
-            },
-            fontFamily: {
-                sans: ['"Rubik Variable"', ...defaultTheme.fontFamily.sans],
-                mono: ['"JetBrains Mono Variable"', ...defaultTheme.fontFamily.mono]
-            },
-            fontWeight: { inherit: 'inherit' },
-            keyframes: {
-                'fade-in': {
-                    from: { opacity: '0%' },
-                    to: { opacity: '100%' }
-                }
-            },
-            animation: { 'fade-in': 'fade-in 1s linear 1' },
-            typography: ({ theme }: { theme: (val: string) => string }) => ({
-                gray: {
-                    css: {
-                        '--tw-prose-invert-body': theme('colors.white')
-                    }
-                }
-            })
+  darkMode: 'class',
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+    './nuxt.config.ts'
+  ],
+  plugins: [typography],
+  theme: {
+    extend: {
+      colors: {
+        primary: theme.primary,
+        secondary: 'hsla(215,8%,47%,.4)',
+        white: theme.body,
+        background: theme.background
+      },
+      fontFamily: {
+        sans: ['"Rubik Variable"', ...defaultTheme.fontFamily.sans],
+        mono: ['"JetBrains Mono Variable"', ...defaultTheme.fontFamily.mono]
+      },
+      fontWeight: { inherit: 'inherit' },
+      keyframes: {
+        'fade-in': {
+          from: { opacity: '0%' },
+          to: { opacity: '100%' }
         }
+      },
+      animation: { 'fade-in': 'fade-in 1s linear 1' },
+      typography: ({ theme }: { theme: (val: string) => string }) => ({
+        gray: {
+          css: {
+            '--tw-prose-invert-body': theme('colors.white')
+          }
+        }
+      })
     }
+  }
 };
 
 export default config;
