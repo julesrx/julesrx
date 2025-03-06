62fc02e7f8f211b2f814b68254fc04ab852ea01a
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2025-03-06 21:46:27 +0100

    chore: update prettier rules (#25)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,53 @@
 const title = 'Jules Raffoux · Tech lead';
 const description =
-    'Tech lead based in Bordeaux, working with .NET, Vue, Typescript, Go and other recent stuff.';
+  'Tech lead based in Bordeaux, working with .NET, Vue, Typescript, Go and other recent stuff.';
 const url = 'https://julesrx.fr/';
 
 export default defineNuxtConfig({
-    modules: ['@nuxt/content', '@nuxt/image', '@vueuse/nuxt'],
-    image: { domains: ['avatars.githubusercontent.com'] },
-    css: [
-        '@fontsource-variable/rubik/index.css',
-        '@fontsource-variable/jetbrains-mono/index.css',
-        '~/assets/main.css'
-    ],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
-    },
-    app: {
-        pageTransition: { name: 'page', mode: 'out-in' },
-        head: {
-            htmlAttrs: { lang: 'en' },
-            title,
-            meta: [
-                { name: 'title', content: title },
-                { name: 'description', content: description },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'theme-color', content: '#2ecc71' },
-                { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
-
-                // Twitter
-                { name: 'twitter:card', content: 'summary' },
-                { name: 'twitter:title', content: title },
-                { name: 'twitter:description', content: description },
-                { name: 'twitter:image', content: url + 'summary.png' },
-
-                // Open Graph / Facebook
-                { name: 'og:title', content: title },
-                { name: 'og:type', content: 'website' },
-                { name: 'og:image', content: url + 'summary.png' },
-                { name: 'og:url', content: url },
-                { name: 'og:description', content: description },
-                { name: 'og:locale', content: 'en_GB' }
-            ],
-            link: [
-                { rel: 'canonical', href: url },
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-                { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
-            ]
-        }
+  modules: ['@nuxt/content', '@nuxt/image', '@vueuse/nuxt'],
+  image: { domains: ['avatars.githubusercontent.com'] },
+  css: [
+    '@fontsource-variable/rubik/index.css',
+    '@fontsource-variable/jetbrains-mono/index.css',
+    '~/assets/main.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
     }
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      htmlAttrs: { lang: 'en' },
+      title,
+      meta: [
+        { name: 'title', content: title },
+        { name: 'description', content: description },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#2ecc71' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+
+        // Twitter
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:image', content: url + 'summary.png' },
+
+        // Open Graph / Facebook
+        { name: 'og:title', content: title },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:image', content: url + 'summary.png' },
+        { name: 'og:url', content: url },
+        { name: 'og:description', content: description },
+        { name: 'og:locale', content: 'en_GB' }
+      ],
+      link: [
+        { rel: 'canonical', href: url },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
 const classes = [
-    'mt-4',
-    'prose',
-    'prose-invert',
-    'prose-gray',
-    'prose-h3:text-2xl',
-    'prose-a:no-underline',
-    'prose-a:text-primary',
-    'prose-a:font-inherit',
-    'prose-img:my-0',
-    'prose-img:opacity-90',
-    'prose-li:m-0',
-    'prose-code:bg-secondary',
-    'prose-code:before:content-none',
-    'prose-code:after:content-none',
-    'prose-code:rounded',
-    'prose-code:font-light',
-    'prose-ul:mb-4',
-    'prose-p:my-4',
-    'prose-headings:font-medium',
-    'prose-h4:mt-4'
+  'mt-4',
+  'prose',
+  'prose-invert',
+  'prose-gray',
+  'prose-h3:text-2xl',
+  'prose-a:no-underline',
+  'prose-a:text-primary',
+  'prose-a:font-inherit',
+  'prose-img:my-0',
+  'prose-img:opacity-90',
+  'prose-li:m-0',
+  'prose-code:bg-secondary',
+  'prose-code:before:content-none',
+  'prose-code:after:content-none',
+  'prose-code:rounded',
+  'prose-code:font-light',
+  'prose-ul:mb-4',
+  'prose-p:my-4',
+  'prose-headings:font-medium',
+  'prose-h4:mt-4'
 ];
 </script>
 
 <template>
-    <div>
-        <ContentDoc id="readme" path="/readme" tag="article" :class="classes" :head="false" />
-    </div>
+  <div>
+    <ContentDoc id="readme" path="/readme" tag="article" :class="classes" :head="false" />
+  </div>
 </template>
